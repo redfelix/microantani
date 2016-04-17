bff51ff7cd583b038d0e751e8e09d9fb57f9cb80
redfelix <felicedanielerusso@gmail.com>
2016-04-17 11:14:32 +0200

    Update felice_russo.py

--- felice_russo.py
+++ felice_russo.py
@@ -1,12 +1,11 @@
-
-import microbit
+from microbit import *
 while True:                                     
     microbit.display.scroll("Hello python")
     microbit.sleep(100)
 
-    if button_a.was_pressed():
+    if button_a.is_pressed():
         break
-    elif button_b.was_pressed():
+    elif button_b.is_pressed():
         display.show(Image.SAD)
  
 
