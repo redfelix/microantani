19746afbb29e93ab9a2c669f967026a4dbbd8948
redfelix <felicedanielerusso@gmail.com>
2016-04-17 11:30:03 +0200

    Update felice_russo.py

--- felice_russo.py
+++ felice_russo.py
@@ -1,12 +1,21 @@
 from microbit import *
-while True:                                     
-    microbit.display.scroll("Hello python")
+import random
+
+while True:
+	microbit.display.scroll("Benvenuti")
     microbit.sleep(100)
 
+	nomi = ["Pippo","Pluto","Paperino"]
+	cose = ["Chitarra","Mandolino","pianoforte"]
+
+	a = random.choice(nomi)
+	b = random.choice(cose)
+
     if button_a.is_pressed():
-        break
+		display.scroll(a)
+		microbit.sleep(100)
+		display.scroll(b)
+		microbit.sleep(300)
     elif button_b.is_pressed():
-        display.show(Image.SAD)
- 
-
-
+        display.scroll("ciaoo")
+        microbit.sleep(300)
